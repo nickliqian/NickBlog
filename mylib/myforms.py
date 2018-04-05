03724911ddd6d4b901e0cdf67cda44be43831b0f
nick windows <nickliqian@outlook.com>
2018-04-05 17:41:14 +0800

    update user profile page

--- mylib/myforms.py
+++ mylib/myforms.py
@@ -1,5 +1,4 @@
 from django.forms import ModelForm, Textarea, CharField, Form
-from DjangoUeditor.models import UEditorField
 from article.models import Comment
 
 from django.contrib.auth.forms import UserCreationForm
@@ -12,14 +11,10 @@ class CommentForm(ModelForm):
         fields = ['article', 'userOfComment', 'content']
         widgets = {
             'content': Textarea(attrs={'cols': 80, 'rows': 5}),
-            # 'content': CharField(label="内容", widget=UEditorWidget(width=1000, height=300, imagePath="uploads/blog/images/", filePath='uploads/blog/files/', toolbars='besttome')),
         }
 
 
 class CustomForm(Form):
-    # description = UEditorField(verbose_name='abc', width=600, height=300, toolbars="full",
-    #                             imagePath="uploads/blog/images/", filePath="uploads/blog/files/")
-    # Description=UEditorField("描述",initial="abc",width=600,height=800)
     pass
 
 class RegisterForm(UserCreationForm):
